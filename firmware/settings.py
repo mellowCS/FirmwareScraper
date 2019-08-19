162adc40cd4306f204346f13ce2124203767eeef
mellowCS <cs.germany@protonmail.com>
2019-08-19 09:57:19 +0200

    set new download timeout of 320 seconds

--- firmware/settings.py
+++ firmware/settings.py
@@ -28,6 +28,7 @@ ROBOTSTXT_OBEY = True
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 #DOWNLOAD_DELAY = 3
+DOWNLOAD_TIMEOUT = 320
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
