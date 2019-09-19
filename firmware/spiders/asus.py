2932a148a9110f6eab7bf44fe0a634f239263bcb
lwilms <45386825+lwilms@users.noreply.github.com>
2019-09-19 10:26:11 +0200

    update asus

--- firmware/spiders/asus.py
+++ firmware/spiders/asus.py
@@ -6,24 +6,23 @@ from scrapy.loader import ItemLoader
 # -*- coding: utf-8 -*-
 from scrapy.http import Request
 from bs4 import BeautifulSoup
+# from ..driver import FACT_Dictionary
 
 class AsusSpider(Spider):
     name = 'asus'
-    # allowed_domains = ['asus.com']
-    # itemname = 'dat_item'
-    manufacturer = 'ASUSTeK Computer Inc.'
+    manufacturer = 'ASUS'
     device_dictionary = dict(
-        gt="Gaming-Router",
-        rt="Router",
+        gt="Router (Home)",  # Gaming
+        rt="Router (Home)",
         rp="Repeater",
         ea="Access Point",
-        ly="Mesh-Router",
-        bl="Mesh-Router",
-        ds="Modem-Router",
-        pc="PCIe-Netzwerkkarte",
-        us="USB-Netzwerkkarte",
+        ly="Router (Home)",  # Mesh
+        bl="Router (Home)",  # Mesh
+        ds="Router (Modem)",  # Modem
+        pc="PCIe-Networkcard",
+        us="USB-Networkcard",
         bt="Bluetooth-Adapter",
-        br="Business-Router"
+        br="Router (Business)"
     )
     base_url = 'https://www.asus.com/de/%s/AllProducts/'
     start_urls = [
@@ -32,50 +31,61 @@ class AsusSpider(Spider):
         base_url % 'Commercial-Gaming-Station',
         base_url % 'Commercial-Servers-Workstations'
     ]
-    # Motherboards, Networking,
 
-    # todo doppelpacks und Lyra mini, eventuell Source Code, vlt fragen, was noch wichtig ist?
+    # todo eventuell Source Code?
     def parse(self, response):
-
         for product_anchor in response.xpath('//div[@class="product_level_1"]/table/tbody/tr/td/ul/li/a').getall():
             product_name, product_link = self.extract_anchor_attributes(product_anchor)
+            if 'AiMesh' in product_name:
+                continue
 
             yield Request(
                 url='https://www.asus.com%sHelpDesk_BIOS/' % product_link,
                 meta={'selenium': True,
                       'dont_redirect': True,
-                      'handle_httpstatus_list': [302]
+                      'handle_httpstatus_list': [302],
+                      'asus': True
                       },
                 callback=self.parse_firmware,
                 cb_kwargs=dict(product_name=product_name)
             )
-            # as of scrapy 1.7 cb_kwargs=dict(productname=productname)
 
     def parse_firmware(self, response, product_name):
         # firmware: 'https://www.asus.com/Networking/RT-AX88U/HelpDesk_BIOS/'
-        download_link = response.xpath('//div[@class="download-inf-r"]/a/@href').get()
-        item_loader_class = ItemLoader(
-            item=FirmwareItem(),
-            response=response,
-            date_fmt=["%Y/%m/%d"]
-        )
+        meta_data = self.prepare_meta_data(response, product_name)
+        return self.prepare_item_pipeline(meta_data=meta_data)
 
-        item_loader_class.add_value("file_urls", [download_link])
-        item_loader_class.add_value("device_name", product_name)
-        item_loader_class.add_value('vendor', self.manufacturer)
-        item_loader_class.add_value('firmware_version', self.extract_firmware_version(response))
-        item_loader_class.add_value('device_class', self.extract_device_class(product_name))
-        item_loader_class.add_value('release_date', self.extract_realese_date(response))
+    @staticmethod
+    def prepare_item_pipeline(response, meta_data):
+        item_loader_class = ItemLoader(item=FirmwareItem(), response=response, date_fmt=['%Y-%m-%d'])
+
+        item_loader_class.add_value('device_name', meta_data['device_name'])
+        item_loader_class.add_value('vendor', meta_data['vendor'])
+        item_loader_class.add_value('firmware_version', meta_data['firmware_version'])
+        item_loader_class.add_value('device_class', meta_data['device_class'])
+        item_loader_class.add_value('release_date', meta_data['release_date'])
+        item_loader_class.add_value("file_urls", meta_data['file_urls'])
 
         return item_loader_class.load_item()
+
+    def prepare_meta_data(self, response, product_name):
+        return {
+            'vendor': 'asus',
+            'release_date': self.extract_release_date(response),
+            'device_name': product_name,
+            'firmware_version': self.extract_firmware_version(response),
+            'device_class': self.extract_device_class(response.url, product_name),
+            'file_urls': response.xpath('//div[@class="download-inf-r"]/a/@href').get()}
 
     @staticmethod
     def extract_anchor_attributes(product_anchor):
         soup = BeautifulSoup(product_anchor, "lxml")
         product_link = soup.a.get('href')
         product_name = soup.a.get_text()
-        return product_name, product_link
+        if 'ROG Rapture' in product_name:
+            product_name = product_name.replace('ROG Rapture ', '')
 
+        return product_name, product_link
 
     @staticmethod
     def extract_firmware_version(response):
@@ -83,23 +93,30 @@ class AsusSpider(Spider):
         if not firmware_version:
             firmware_version = None
 
-        if response.xpath('//span[@class="beta"]').extract():
-            firmware_version = firmware_version + ' beta'
+        if 'Beta' in response.xpath('//span[@class="beta"]/text()').get():
+            firmware_version = firmware_version + '-beta'
 
-        return firmware_version
+        return firmware_version.replace('Version ', '').replace(' ', '_')
 
     @staticmethod
-    def extract_realese_date(response):
+    def extract_release_date(response):
         release_date = response.xpath('//span[@class="lastdate"]/text()').get()
         if not release_date:
-            release_date =
+            release_date = None
 
-        return release_date
+        return release_date.replace('/', '-')
 
-    def extract_device_class(self, product_name):
-        if product_name[:2].lower() in self.device_dictionary:
-            device_class = self.device_dictionary[product_name[:2].lower()]
+    def extract_device_class(self, response_url, product_name):
+        if 'Motherboards' in response_url:
+            device_class = 'Motherboard'
+
+        elif 'Commercial-Gaming-Station' in response_url:
+            device_class = 'BIOS'
+
+        elif 'Networking' in response_url:
+            if product_name[:2].lower() in self.device_dictionary:
+                device_class = self.device_dictionary[product_name[:2].lower()]
         else:
-            device_class = product_name
+            device_class = None
 
         return device_class
