19db698b667ac5f46d124283fdf0e022dcb68947
lwilms <45386825+lwilms@users.noreply.github.com>
2019-11-04 15:43:31 +0100

    removed the comments

--- firmware/items.py
+++ firmware/items.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 from scrapy.item import Item, Field
 
 
@@ -15,6 +8,5 @@ class FirmwareItem(Item):
     device_class = Field(default=None)
     release_date = Field(default=None)
 
-    # used by FilesPipeline
     files = Field()
     file_urls = Field()
