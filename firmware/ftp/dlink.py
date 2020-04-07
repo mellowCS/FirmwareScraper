f484831523df5fdd5a57c3b88e83267a80312994
lwilms <lwilms@posteo.de>
2020-04-07 13:23:38 +0200

    cosmetics and remove donwload of deprecated files

--- firmware/ftp/dlink.py
+++ firmware/ftp/dlink.py
@@ -62,25 +62,29 @@ class FTPClass:
     def download_directory(self, device_name):
 
         for (file_name, file_details) in self.start_iteration():
-            if not re.search('zip$', file_name) or (is_file(file_name) and stat(file_name).st_size == file_details['size']):
+            if not re.search('zip$', file_name) or (
+                    is_file(file_name) and stat(file_name).st_size == file_details['size']):
                 continue
 
             if '_fw_' in file_name:
                 try:
-                    device_class = self.device_classes_dict[device_name.split('-')[0]]
+                    device_initials = device_name.split('-')[0]
+                    device_class = self.device_classes_dict[device_initials]
+                    if device_initials == 'dwl' and 'ap' in device_name:
+                        device_class = 'Access Point'
                 except Exception as e:
                     device_class = None
-                    self.LOG(e, self.ftp_client.pwd())
+                    self.LOG(e)
                 try:
                     release_date = datetime.timestamp(datetime.strptime(file_details['modify'], "%Y%m%d%H%M%S"))
                 except Exception as e:
                     release_date = None
-                    self.LOG(e, self.ftp_client.pwd())
+                    self.LOG(e)
                 try:
-                    firmware_version = file_name.split('_')[3]  # todo
+                    firmware_version = file_name.split('_')[3]
                 except Exception as e:
                     firmware_version = None
-                    self.LOG(e, self.ftp_client.pwd())
+                    self.LOG(e)
 
                 self.json_file.append(
                     {'device_name': device_name,
@@ -109,99 +113,58 @@ class FTPClass:
         next(site_columns)
         return site_columns
 
-    @staticmethod
-    def LOG(error, directory_name):
+    def LOG(self, error):
         with open('logfile.txt', 'a') as logfile:
-            logfile.write('Errormessage: {} \n Directory: {}\n'.format(error, directory_name))
+            logfile.write('Errormessage: {} \n Directory: {}\n'.format(error, self.ftp_client.pwd()))
 
 
 if __name__ == '__main__':
     ftp_address = 'ftp.dlink.de'
 
-    things_to_skip = {'@archive', 'anleitungen',
-                      'ant24', 'ant70',
-                      # Antennen - keine Firmware vorhanden, soweit die manuelle Prüfung es ergeben hat
-                      'D-Link_Assist_Anleitung.pdf', 'Hinweise Datenblaetter.txt', 'Images_High_Resolution',
-                      'Images_Low_Resolution', 'index_info.txt', 'Legal - Information', 'Product_Images',
-                      'Product_Information_Material', 'self - service', 'software',
-                      'Supportsystem_Anleitung_Mass_RMA.pdf', 'Terms_and_Conditions', 'tmp', 'Warranty_Documents',
-                      # deprecated
-                      'dcf', 'de', 'dfw', 'dhd', 'dif', 'dm', 'dph', 'dvc', 'dvg'
-                      }
-    # todo start threads here? maybe skip every 1,2,3,4 and so on OR do it with a queue
-    # get element from queue, check for things to skip, get new one or connect
-    name_dictionary = {}
-    for name in {'covr', 'dgl', 'dir', 'dva'}:  # covr = smart home, dir = mesh? dva = HorstBox
+    things_to_skip = {
+        '@archive', 'anleitungen', 'D-Link_Assist_Anleitung.pdf', 'Hinweise Datenblaetter.txt',
+        'Images_High_Resolution', 'Images_Low_Resolution', 'index_info.txt', 'Legal - Information', 'Product_Images',
+        'Product_Information_Material', 'self - service', 'software', 'Supportsystem_Anleitung_Mass_RMA.pdf',
+        'Terms_and_Conditions', 'tmp', 'Warranty_Documents',
+        # deprecated systems
+        'ant24', 'ant70', 'dcf', 'de', 'dfw', 'dhd', 'dif', 'dm', 'dph', 'dvc', 'dvg', 'dvg', 'dta', 'dsn', 'dsm',
+        'dns', 'dvs', 'dfl', 'dbt', 'dev', 'dcm', 'dgl', 'dhs', 'di', 'dws', 'dfe', 'du'
+    }
+
+    name_dictionary = {
+        'dsr': 'Router (Business)', 'dwc': 'Wireless Controller', 'dsl': 'Router (Modem)', 'dnr': 'Video Recorder',
+        'dps': 'Redundant Power Supply', 'dsp': 'Smart Plug', 'dub': 'USB Extensions', 'dem': 'Transceiver',
+        'dwl': 'Wireless LAN'
+    }
+    # for dwl: ap = Access Point, e = enterprise, s = small to medium business, g = SuperG?!, m = MIMO,
+    # p = power over ethernet, plus = 802.11b+, ag = 802.11a and 802.11g, else PCIe, Adapter many more
+
+    for name in {'covr', 'dir', 'dva', 'go'}:  # go-plk = powerline connection, go-dsl = modem-router
         name_dictionary[name] = 'Router (Home)'
-    for name in {'dsr'}:
-        name_dictionary[name] = 'Router (Business)'
-    for name in {'dcm', 'dsl'}:
-        name_dictionary[name] = 'Router (Modem)'
-    for name in {'dwr'}:
+    for name in {'dwr', 'dwm'}:
         name_dictionary[name] = 'Router (mobile)'
     for name in {'dba', 'dap', 'dwl'}:
         name_dictionary[name] = 'Access Point'
-    for name in {}:
-        name_dictionary[name] = 'Wireless Controller'
-    for name in {}:
-        name_dictionary[name] = 'Repeater'
-    for name in {'des', 'dgs', 'dhs', 'di', 'dkvm', 'dqs'}:  # dkvm = kvm switch, dqs = enterprise data center switch
+    for name in {'des', 'dgs', 'dkvm', 'dqs', 'dxs'}:
         name_dictionary[name] = 'Switch'
-    for name in {'dev'}:
-        name_dictionary[name] = 'Bridge'
-    for name in {}:
-        name_dictionary[name] = 'USB-Networkcard'
-    for name in {'dfe', 'dge'}:
+    for name in {'dge', 'dwa', 'dxe'}:
         name_dictionary[name] = 'PCIe-Networkcard'
-    for name in {'dbt'}:
-        name_dictionary[name] = 'Bluetooth-Adapter'
     for name in {'dnc', 'cwm', 'ds', 'dv'}:
         name_dictionary[name] = 'Software'
-    for name in {'dcf', 'de', 'dfw', 'dhd', 'dif', 'dm', 'dph', 'dvc', 'dvg'}:
-        # todo outdated: to check
-        #  dcf = 'bt' = bluetooth, 'w' = wireless-Adapter,
-        #  de = ethernetcable-adapter
-        #  dfw = PCI Adapter
-        #  dhd = TV-Wifi-Adapter zum streamen
-        #  dif = _
-        #  dm = _
-        #  dph = phone/phone adapter
-        #  dvc = videophone
-        #  dvg = VoIP Station Gateway?
-        # TODO DVG last one!
-        name_dictionary[name] = 'Wi-Fi Water Sensor'
     for name in {'dcs', 'dsh'}:
         name_dictionary[name] = 'Smart Wi-Fi Camera'
-    for name in {'dem'}:
-        name_dictionary[name] = 'Transceiver'
-    for name in {'dfl'}:  # "NetDefend
-        name_dictionary[name] = 'Business Firewall'
     for name in {'dis', 'dmc'}:
-        name_dictionary[name] = 'Converter'  # ethernet, media etc. glasfaser zu trashfaser
-    for name in {'dnr'}:
-        name_dictionary[name] = 'Video Recorder'
-    for name in {'dns'}:
-        name_dictionary[name] = 'Network Attached Storage'
-    for name in {'dps'}:
-        name_dictionary[name] = 'Redundant Power Supply'
-    for name in {'dsm'}:
-        name_dictionary[name] = 'Media Player'
-    for name in {'dsn'}:
-        name_dictionary[name] = 'Storage Array'
-    for name in {'dsp'}:
-        name_dictionary[name] = 'Smart Powr Supply'
-    for name in {'dta'}:
-        name_dictionary[name] = 'ISDN Card'
-    for name in {'du', 'dub'}:  # PCI card & extern & card
-        name_dictionary[name] = 'USB Extensions'
+        name_dictionary[name] = 'Converter'
 
-    # create own Downloads directory
     downloads_directory = 'firmware_files'
     if is_directory(downloads_directory):
         chdir(downloads_directory)
     else:
         mkdir(downloads_directory)
         chdir(downloads_directory)
+
+    # todo start threads here? maybe skip every 1,2,3,4 and so on OR do it with a queue
+    #  get element from queue, check for things to skip, get new one or connect
 
     max_thread = 1
     for number in range(max_thread):
