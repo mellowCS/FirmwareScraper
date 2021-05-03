0f8cf5a1ca69c2636676d20942721111bfc307d8
lwilms <lwilms@posteo.de>
2021-05-03 16:00:18 +0200

    running on us, updated https, date and firmware path

--- firmware/spiders/linksys.py
+++ firmware/spiders/linksys.py
@@ -33,11 +33,12 @@ class LinksysSpider(Spider):
         ClassIdentifier(['LACX', 'LACG']): 'Transceiver',
         ClassIdentifier(['LAPN', 'LAPAC']): 'Business Access Point',
         ClassIdentifier(['LCA']): 'Business Camera',
-        ClassIdentifier(['LMR']): 'Business Video Recorder',
+        ClassIdentifier(['LMR', 'LNR']): 'Business Video Recorder',
         ClassIdentifier(['LNE', 'EG', 'WMP']): 'PCI Network Adapter',
         ClassIdentifier(['LRT']): 'VPN Router',
         ClassIdentifier(['LGS']): 'Business Switch',
         ClassIdentifier(['MR', 'EA', 'WRT', 'E', 'BEF', 'WKU', 'WRK']): 'Router',
+        ClassIdentifier(['M10', 'M20']): 'Hotspot',
         ClassIdentifier(['NMH']): 'Media Hub',
         ClassIdentifier(['NSL']): 'Network Storage Link',
         ClassIdentifier(['PCM']): 'CardBus PC Card',
@@ -47,8 +48,8 @@ class LinksysSpider(Spider):
         ClassIdentifier(['WAP']): 'Home Access Point',
         ClassIdentifier(['WET', 'WUM', 'WES']): 'Bridge',
         ClassIdentifier(['WGA', 'WMA', 'WPC']): 'Wireless Adapter',
-        ClassIdentifier(['WHW', 'VLP']): 'Wifi Mesh System',
-        ClassIdentifier(['WMC']): 'Home Camera',
+        ClassIdentifier(['WHW', 'VLP', 'MX']): 'Wifi Mesh System',
+        ClassIdentifier(['WMC', 'WVC']): 'Home Camera',
         ClassIdentifier(['WML']): 'Music System',
         ClassIdentifier(['WUSB', 'USB', 'AE']): 'Wifi USB Adapter',
         ClassIdentifier(['X', 'AG', 'WAG']): 'Modem Router'
@@ -57,20 +58,24 @@ class LinksysSpider(Spider):
     x_path = {
         'product_urls': '//div[@class="item"]//@href',
         'device_names': '//div[@class="item"]//a/text()',
-        'software_exists': '//div[@class="support-downloads col-sm-6"]//a[@title="Software herunterladen"]/@href',
+        'software_exists': '//div[@class="support-downloads col-sm-6"]//a[@title="Download Software"]/@href',
+        # german text: Software herunterladen
         'firmware': '//div[@id="support-article-downloads"]/div[@class="article-accordian-content collapse-me"]',
     }
 
-    start_urls = ['https://www.linksys.com/de/support/sitemap/']
+    start_urls = ['https://www.linksys.com/us/support/sitemap/']
 
     def parse(self, response: Response) -> Generator[Request, None, None]:
-        for product_url, device_name in list(zip(response.xpath(self.x_path['product_urls']).extract(), response.xpath(self.x_path['device_names']).extract())):
-            yield Request(url=response.urljoin(product_url), callback=self.parse_product, cb_kwargs=dict(device_name=device_name))
+        for product_url, device_name in list(zip(response.xpath(self.x_path['product_urls']).extract(),
+                                                 response.xpath(self.x_path['device_names']).extract())):
+            yield Request(url=response.urljoin(product_url), callback=self.parse_product,
+                          cb_kwargs=dict(device_name=device_name))
 
     def parse_product(self, response: Response, device_name: str) -> Generator[Request, None, None]:
         software_page = response.xpath(self.x_path['software_exists']).get()
         if software_page:
-            yield Request(url=response.urljoin(software_page), callback=self.parse_versions, cb_kwargs=dict(device_name=device_name))
+            yield Request(url=response.urljoin(software_page), callback=self.parse_versions,
+                          cb_kwargs=dict(device_name=device_name))
 
     def parse_versions(self, response: Response, device_name: str) -> Generator[FirmwareItem, None, None]:
         for version in response.xpath(self.x_path['firmware']).extract():
@@ -78,9 +83,11 @@ class LinksysSpider(Spider):
 
     def parse_urls(self, device_name: str, version: str) -> Generator[FirmwareItem, None, None]:
         self.PRODUCT_DICTIONARIES = list()
-        for firmware in re.findall(r'(Ver\.(?:.|\n)*?href=\"http://downloads\.linksys\.com/downloads/firmware/[\w\.]+\")', version):
+        for firmware in re.findall(r'Ver.+href=\".+(?:bin|img)\"', version):
             if re.search(r'(\.img|\.bin)', firmware):
-                yield from self.parse_firmware(meta_data=self.prepare_meta_data(firmware=firmware, device_name=device_name, device_class=self.map_device_class(device_name)))
+                yield from self.parse_firmware(
+                    meta_data=self.prepare_meta_data(firmware=firmware, device_name=device_name,
+                                                     device_class=self.map_device_class(device_name)))
 
     def parse_firmware(self, meta_data: dict) -> Generator[FirmwareItem, None, None]:
         if meta_data not in self.PRODUCT_DICTIONARIES:
@@ -104,11 +111,14 @@ class LinksysSpider(Spider):
         match = re.search(r'href="(.*\.bin|.*\.img)"', firmware)
         file_urls = match.group(1) if match else 'N/A'
 
-        match = re.search(r'Ver\.([^<([a-zA-Z]+]*)', firmware)
-        version = match.group(1).strip(' ') if match else 'N/A'
+        match = re.search(r'(?:Ver|Version)\.([^<([a-zA-Z]+]*)', firmware)
+        version = match.group(1).strip(' ').replace('\xa0', '') if match else 'N/A'
 
-        match = re.search(r'([^\d]+)(\d+/\d+/\d{4}).*', firmware)
-        date = datetime.strptime(match.group(2), "%m/%d/%Y").strftime("%Y-%m-%d") if match else 'N/A'
+        match = re.search(
+            r'((?:[1-9]|0[1-9]|10|11|12)(?:\s|\.|/|-)(?:[a-zA-Z]+|[1-9]|[1-2][0-9]|30|31)(?:\s|\.|/|-)(?:20|19)\d{2})',
+            firmware)
+        date = datetime.strptime(match.group(1).replace(' ', '/').replace('\xa0', '/'), r"%m/%d/%Y").strftime(
+            "%Y-%m-%d") if match else 'N/A'
 
         return dict(file_urls=file_urls, vendor='Linksys', device_name=device_name,
                     firmware_version=version, device_class=device_class, release_date=date)
@@ -119,4 +129,5 @@ class LinksysSpider(Spider):
                 if product.startswith(shortcut):
                     return self.device_classes[identifiers]
 
-        raise UnknownDeviceClassException('The product: {} cannot be found in the Device Class dictionary.'.format(product))
+        raise UnknownDeviceClassException(
+            'The product: {} cannot be found in the Device Class dictionary.'.format(product))
