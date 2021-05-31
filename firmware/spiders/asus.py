9811927e81671a8e81089d2e8a14b32ec5d4b6ac
lwilms <lwilms@posteo.de>
2021-05-31 17:25:44 +0200

    update ASUS scrawler

--- firmware/spiders/asus.py
+++ firmware/spiders/asus.py
@@ -1,8 +1,8 @@
-from bs4 import BeautifulSoup
+from datetime import datetime
+
 from scrapy import Spider
-from scrapy.http import Request
-from scrapy.loader import ItemLoader
 from scrapy.exceptions import NotSupported
+from scrapy.loader import ItemLoader
 
 from firmware.items import FirmwareItem
 
@@ -21,37 +21,37 @@ class AsusSpider(Spider):
         pc='PCIe-Networkcard',
         us='USB-Networkcard',
         bt='Bluetooth-Adapter',
-        br='Router (Business)'
+        br='Router (Business)',
+        es='Server',
+        rs='Server',
+        ro='Router (Gaming)'  # ROG Rapture
     )
-    base_url = 'https://www.asus.com/de/{}/AllProducts/'
+    base_url = 'https://www.asus.com/de/Networking-IoT-Servers/{}/All-series/filter/'
     start_urls = [
-        base_url.format('Networking'),
-        base_url.format('Motherboards'),
-        base_url.format('Commercial-Gaming-Station'),
-        base_url.format('Commercial-Servers-Workstations')
+        base_url.format('WiFi-Routers'),
+        base_url.format('Modem-Routers'),
+        base_url.format('WiFi-6')
     ]
 
     def parse(self, response):
-        for product_anchor in response.xpath('//div[@class="product_level_1"]/table/tbody/tr/td/ul/li/a').getall():
-            product_name, product_link = self.extract_anchor_attributes(product_anchor)
-            if 'AiMesh' in product_name:
+        for url_redirect in set(response.xpath('//div[contains(@class, "ProductCardNormal")]//a/@href').getall()):
+            if url_redirect[-1] != '/':
                 continue
-
-            yield Request(
-                url='https://www.asus.com%sHelpDesk_BIOS/' % product_link,
+            response.follow(response.url)
+            yield response.follow(
+                url=f'{url_redirect}HelpDesk_BIOS/',
                 meta={'selenium': True,
                       'dont_redirect': True,
                       'handle_httpstatus_list': [302],
                       'asus': True
                       },
-                callback=self.parse_firmware,
-                cb_kwargs=dict(product_name=product_name)
+                callback=self.parse_firmware
             )
 
-    def parse_firmware(self, response, product_name):
-        meta_data = self.prepare_meta_data(response, product_name)
+    def parse_firmware(self, response):
+        meta_data = self.prepare_meta_data(response)
         if meta_data['file_urls'] is None:
-            raise NotSupported
+            return []
         return self.prepare_item_pipeline(response=response, meta_data=meta_data)
 
     @staticmethod
@@ -67,50 +67,33 @@ class AsusSpider(Spider):
 
         return item_loader_class.load_item()
 
-    def prepare_meta_data(self, response, product_name):
+    def prepare_meta_data(self, response):
+        product_name = response.xpath('//h1[contains(@class, "productTitle")]/text()').get()
         return {
             'vendor': 'asus',
             'release_date': self.extract_release_date(response),
             'device_name': product_name,
             'firmware_version': self.extract_firmware_version(response),
             'device_class': self.extract_device_class(response.url, product_name),
-            'file_urls': response.xpath('//div[@class="download-inf-r"]/a/@href').get()
+            'file_urls':
+                response.xpath('//div[contains(@class,"ProductSupportDriverBIOS__contentRight")]//a/@href').get()
         }
 
     @staticmethod
-    def extract_anchor_attributes(product_anchor):
-        soup = BeautifulSoup(product_anchor, 'lxml')
-        product_link = soup.a.get('href')
-        product_name = soup.a.get_text()
-        if 'ROG Rapture' in product_name:
-            product_name = product_name.replace('ROG Rapture ', '')
-
-        return product_name, product_link
-
-    @staticmethod
     def extract_firmware_version(response):
-        firmware_version = response.xpath('//span[@class="version"]/text()').get()
-        if not firmware_version:
-            return None
-        beta_attribute = response.xpath('//span[@class="beta"]/text()').get()
-        if beta_attribute is not None:
-            firmware_version = firmware_version + '-beta'
-
-        return firmware_version.replace('Version ', '').replace(' ', '_')
+        firmware_version = response.xpath('//div[contains(@class,"ProductSupportDriverBIOS__version")]/text()').get()
+        return firmware_version.replace('Version', '').strip() if firmware_version else None
 
     @staticmethod
     def extract_release_date(response):
-        release_date = response.xpath('//span[@class="lastdate"]/text()').get()
-        if not release_date:
-            return None
-
-        return release_date.replace('/', '-')
+        release_date = response.xpath('//div[contains(@class,"ProductSupportDriverBIOS__releaseDate")]/text()').get()
+        return datetime.strptime(release_date.strip(), '%Y/%m/%d').date().isoformat() if release_date else None
 
     def extract_device_class(self, response_url, product_name):
+        if product_name[:2].lower() in self.device_dictionary:
+            return self.device_dictionary[product_name[:2].lower()]
         if 'Motherboards' in response_url:
             return 'Motherboard'
         if 'Commercial' in response_url:
             return 'BIOS'
-        if product_name[:2].lower() in self.device_dictionary:
-            return self.device_dictionary[product_name[:2].lower()]
         return None  # undefined
