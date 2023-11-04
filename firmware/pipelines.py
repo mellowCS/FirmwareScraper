ccc1cf5b9c6df8e06301121cb647f2bad6a8546c
andr3colonel <andr3colonel@gmail.com>
2023-11-05 01:25:04 +0200

    FIR-12: Add new categories and hw version to tp downloader

--- firmware/pipelines.py
+++ firmware/pipelines.py
@@ -1,5 +1,15 @@
+import os
+from hashlib import sha256
+
 from scrapy.pipelines.files import FilesPipeline
 
+
+class StoreFirmwarePipeline(FilesPipeline):
+    def file_path(self, request, response=None, info=None, *, item=None):
+        hashsum = sha256(response.body).hexdigest()
+        path = [hashsum[:2], hashsum[2:4], hashsum[4:]]
+        print("OutPath", os.path.join(*path))
+        return os.path.join(*path)
 
 class FirmwarePipeline(FilesPipeline):
     def file_path(self, request, response=None, info=None):
